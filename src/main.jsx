d51508022e280cad2a179db738d2add4e947d604
FEIQUI <adrianantunez@outlook.com>
2025-10-07 17:29:27 -0300

    Create main.jsx

---
+++ src/main.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import SectoCafePedidos from "./SectoCafePedidos";
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <SectoCafePedidos />
+  </React.StrictMode>
+);

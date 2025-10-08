5f43d6005622bc35c2b6f176abe6758fd2d5786c
FEIQUI <adrianantunez@outlook.com>
2025-10-08 14:18:43 -0300

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -3,7 +3,6 @@ import ReactDOM from "react-dom/client";
 import SectoCafePedidos from "./SectoCafePedidos";
 import "./index.css";
 
-
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <SectoCafePedidos />

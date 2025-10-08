ee74516d708d45e5c84c4fd83de58bae9a163fd9
FEIQUI <adrianantunez@outlook.com>
2025-10-08 18:05:49 -0300

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,9 @@
 import React from "react";
-import ReactDOM from "react-dom/client";
-import SectoCafePedidos from "./SectoCafePedidos";
-import "./index.css";
+import { createRoot } from "react-dom/client";
+import SectoCafePedidos from "./SectoCafePedidos.jsx";
+import "./index.css"; // tailwind
 
-ReactDOM.createRoot(document.getElementById("root")).render(
+createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <SectoCafePedidos />
   </React.StrictMode>

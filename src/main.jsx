643c89ccdd0f702b9fba3f174305fae97e8b4309
FEIQUI <adrianantunez@outlook.com>
2026-02-09 10:01:10 -0300

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -15,15 +15,6 @@ function Router() {
 }
 
 
-function Router() {
-  const path = window.location.pathname;
-
-  if (path === "/kitchen") return <Kitchen />;
-  if (path === "/ticket") return <Ticket />;
-
-  return <SectoCafePedidos />;
-}
-
 createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <Router />

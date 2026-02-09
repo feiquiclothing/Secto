d1df9b7c49b879aa86ae489009708cd4e8a7ce5a
FEIQUI <adrianantunez@outlook.com>
2026-02-09 02:06:20 -0300

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { createRoot } from "react-dom/client";
-import SectoCafePedidos from ".SectoCafePedidos.jsx";
+import SectoCafePedidos from "src/SectoCafePedidos.jsx";
 import "./index.css"; // tailwind
 
 createRoot(document.getElementById("root")).render(

014651f523400106c0b030e390fc5aee9feab39a
FEIQUI <adrianantunez@outlook.com>
2026-02-09 09:58:21 -0300

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -4,6 +4,16 @@ import SectoCafePedidos from "./SectoCafePedidos.jsx";
 import Kitchen from "./Kitchen.jsx";
 import Ticket from "./ticket.jsx";
 import "./index.css"; // tailwind
+import Admin from "./Admin.jsx";
+
+function Router() {
+  const path = window.location.pathname;
+  if (path === "/kitchen") return <Kitchen />;
+  if (path === "/ticket") return <Ticket />;
+  if (path === "/admin") return <Admin />;   // <- nuevo
+  return <SectoCafePedidos />;
+}
+
 
 function Router() {
   const path = window.location.pathname;

3a347ef752da4d50366262e64aca344503493e13
FEIQUI <adrianantunez@outlook.com>
2026-02-09 01:59:23 -0300

    Fix import path for SectoCafePedidos component

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { createRoot } from "react-dom/client";
-import SectoCafePedidos from "src/SectoCafePedidos.jsx";
+import SectoCafePedidos from ".SectoCafePedidos.jsx";
 import "./index.css"; // tailwind
 
 createRoot(document.getElementById("root")).render(

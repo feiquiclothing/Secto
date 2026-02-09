4778a331448294761b0edc9b784dc2970569b1db
FEIQUI <adrianantunez@outlook.com>
2026-02-09 02:20:15 -0300

    Implement Router for component navigation

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,21 @@
 import React from "react";
 import { createRoot } from "react-dom/client";
 import SectoCafePedidos from "./SectoCafePedidos.jsx";
+import Kitchen from "./Kitchen.jsx";
+import Ticket from "./ticket.jsx";
 import "./index.css"; // tailwind
+
+function Router() {
+  const path = window.location.pathname;
+
+  if (path === "/kitchen") return <Kitchen />;
+  if (path === "/ticket") return <Ticket />;
+
+  return <SectoCafePedidos />;
+}
 
 createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <SectoCafePedidos />
+    <Router />
   </React.StrictMode>
 );

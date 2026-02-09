283a35d3cf49f7fe37b15cee430980595cf3c3e9
FEIQUI <adrianantunez@outlook.com>
2026-02-09 10:06:34 -0300

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -2,21 +2,20 @@ import React from "react";
 import { createRoot } from "react-dom/client";
 import SectoCafePedidos from "./SectoCafePedidos.jsx";
 import Kitchen from "./Kitchen.jsx";
-import Ticket from "./ticket.jsx";
-import "./index.css"; // tailwind
 import Admin from "./Admin.jsx";
+import Ticket from "./ticket.jsx"; // si tu archivo se llama ticket.jsx, dejalo así
+import "./index.css";
 
-function Router() {
+function AppRouter() {
   const path = window.location.pathname;
   if (path === "/kitchen") return <Kitchen />;
   if (path === "/ticket") return <Ticket />;
-  if (path === "/admin") return <Admin />;   // <- nuevo
+  if (path === "/admin") return <Admin />;
   return <SectoCafePedidos />;
 }
 
-
 createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <Router />
+    <AppRouter />
   </React.StrictMode>
 );

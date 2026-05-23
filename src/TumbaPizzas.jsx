5cd1524d43cb4d3146bf0eec7da3c8412a4bef67
FEIQUI <adrianantunez@outlook.com>
2026-05-23 18:42:04 -0300

    Update TumbaPizzas.jsx

--- src/TumbaPizzas.jsx
+++ src/TumbaPizzas.jsx
@@ -1,106 +1,61 @@
-// ===== MENU =====
 const MENU = [
   {
-    id: "pizzas",
-    name: "PIZZAS",
+    id: "premium",
+    name: "PIZZAS PREMIUM",
     items: [
-      {
-        id: "p01",
-        name: "Margarita - Salsa de tomate | Muzzarella | Albahaca",
-        price: 420,
-        img: "/Photos/pizza1.jpg",
-      },
-
-      {
-        id: "p02",
-        name: "Pepperoni - Salsa de tomate | Muzzarella | Pepperoni",
-        price: 480,
-        img: "/Photos/pizza2.jpg",
-      },
-
-      {
-        id: "p03",
-        name: "Cuatro quesos - Muzzarella | Azul | Parmesano | Provolone",
-        price: 520,
-        img: "/Photos/pizza3.jpg",
-      },
-
-      {
-        id: "p04",
-        name: "Fugazzeta - Muzzarella | Cebolla caramelizada | Orégano",
-        price: 460,
-        img: "/Photos/pizza4.jpg",
-      },
-
-      {
-        id: "p05",
-        name: "Picante - Salsa | Muzzarella | Salame picante | Miel picante",
-        price: 520,
-        img: "/Photos/pizza5.jpg",
-      },
-
-      {
-        id: "p06",
-        name: "Hongos - Muzzarella | Hongos salteados | Verdeo",
-        price: 480,
-        img: "/Photos/pizza6.jpg",
-      },
-
-      {
-        id: "p07",
-        name: "Jamón crudo y rúcula - Muzzarella | Crudo | Rúcula",
-        price: 560,
-        img: "/Photos/pizza7.jpg",
-      },
-
-      {
-        id: "p08",
-        name: "Tumba Especial - Salsa | Muzzarella | Panceta | Cebolla crispy",
-        price: 590,
-        img: "/Photos/pizza8.jpg",
-      },
+      { id: "p01", name: "De la planta - Bechamel de coco | Cebolla | Parmesano vegano | Tomillo", price: 480 },
+      { id: "p02", name: "Cabrío - Rúcula | Queso de cabra | Miel | Nueces", price: 620 },
+      { id: "p03", name: "Cuchillo de palo - Cebo figazza | Romesco | Parmesano", price: 460 },
+      { id: "p04", name: "Testigo falso - Pepperoni | Merkén", price: 580 },
+      { id: "p05", name: "A otra rata - 3 quesos", price: 560 },
+      { id: "p06", name: "Prende tuba - Bondiola | Chimichanga | Nueces", price: 560 },
     ],
   },
-
+  {
+    id: "clasicas",
+    name: "PIZZAS CLÁSICAS",
+    items: [
+      { id: "c01", name: "Atala con alambre - Cebolla | Muzzarella | Tomillo", price: 420 },
+      { id: "c02", name: "Margarita", price: 480 },
+      { id: "c03", name: "La vieja confiable - Muzzarella", price: 420 },
+      { id: "c04", name: "En mi salsa - Marinara", price: 380 },
+      { id: "c05", name: "Fainá", price: 220 },
+    ],
+  },
+  {
+    id: "combos",
+    name: "COMBOS",
+    items: [
+      { id: "co01", name: "Una pizza clásica + Norteña 473cc", price: 520 },
+      { id: "co02", name: "Dos pizzas clásicas + 2 Norteña 473cc", price: 1050 },
+      { id: "co03", name: "Dos pizzas premium + 2 Norteña 473cc", price: 1280 },
+      { id: "co04", name: "Dos pizzas premium + Fainá", price: 990 },
+    ],
+  },
+  {
+    id: "postres",
+    name: "POSTRES",
+    items: [
+      { id: "po01", name: "Húmeda de chocolate", price: 0 },
+      { id: "po02", name: "Budín de pan", price: 0 },
+      { id: "po03", name: "Chocotorta", price: 0 },
+    ],
+  },
+  {
+    id: "extras",
+    name: "EXTRAS",
+    items: [
+      { id: "e01", name: "Dip pesto", price: 0 },
+      { id: "e02", name: "Dip romesco", price: 0 },
+      { id: "e03", name: "Dip chimichanga", price: 0 },
+    ],
+  },
   {
     id: "bebidas",
     name: "BEBIDAS",
     items: [
-      {
-        id: "b01",
-        name: "Agua Salus sin gas 600cc",
-        price: 100,
-      },
-
-      {
-        id: "b02",
-        name: "Agua Salus con gas 600cc",
-        price: 100,
-      },
-
-      {
-        id: "b03",
-        name: "Coca Cola 600cc",
-        price: 100,
-      },
-
-      {
-        id: "b04",
-        name: "Schweppes 600cc",
-        price: 100,
-      },
-
-      {
-        id: "b05",
-        name: "Sprite 600cc",
-        price: 100,
-      },
-
-      {
-        id: "b06",
-        name: "Fanta 600cc",
-        price: 100,
-      },
+      { id: "b01", name: "Línea Coca Cola", price: 140 },
+      { id: "b02", name: "Norteña 473cc", price: 150 },
     ],
   },
 ];

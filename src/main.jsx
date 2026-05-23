2da521f994536e748bafff2bfcc72f213856100f
FEIQUI <adrianantunez@outlook.com>
2026-05-23 18:49:57 -0300

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import { createRoot } from "react-dom/client";
 
 import SectoCafePedidos from "./SectoCafePedidos.jsx";
-import TumbaPizzas from "./TumbaPizzas.jsx";
+import TumbaPizzas from "./tumbapizzas.jsx";
 
 import Kitchen from "./Kitchen.jsx";
 import Admin from "./Admin.jsx";
